6a88b72161ef040783d0d53754aa3eb8fa5b9ecc
Nanateng <nanateng2020@gmail.com>
2022-09-01 23:21:23 +0800

    updated

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const searchInput = document.querySelector('#search-input');
 const searchBtn = document.querySelector('button');
 const mealList = document.querySelector('#meal-card');
 const mealResults = document.querySelector('#result');
+const getRecipe = document.querySelector('#get-recipe');
 
 // nav search bar
 // magnifyingGlass.addEventListener('click', () => {
@@ -20,22 +21,40 @@ function getMeals() {
     .then(res => res.json())
     .then(data => {
       console.log(data);
-      let html = '';
+      let resultList = '';
       if (data.meals) {
         data.meals.forEach(meal => {
-          html += ` 
-           <div class="flex flex-col rounded-xl py-3 meal-card" data-id="${meal.idMeal}" >
-            <div class="w-72 h-72">
-              <img class="rounded-lg object-cover w-full h-full drop-shadow-md" src="${meal.strMealThumb}" alt="food" />
-            </div>
-            <div class="text-center font-body text-xl bg-black bg-opacity-30 rounded-b-md flex flex-col">
-              <span class="font-semibold">${meal.strMeal} </span>
-              <span><a href="" class="text-cyan-300 italic underline leading-relaxed">Get Recipes</a></span>
+          resultList += ` 
+          <div class="flex flex-wrap w-full">
+            <div class="flex flex-col rounded-xl py-3 meal-card" data-id="${meal.idMeal}" >
+                <div class="w-72 h-72">
+                    <img class="rounded-lg object-cover w-full h-full drop-shadow-md" src="${meal.strMealThumb}" alt="food" />
+                </div>
+                <div class="text-center font-body text-xl bg-black bg-opacity-30 rounded-b-md flex flex-col">
+                    <span class="font-semibold">${meal.strMeal} </span>
+                    <span><a href="" class="text-cyan-300 italic underline leading-relaxed">Get Recipes</a></span>
+                </div>
             </div>
           </div>`;
         });
         mealResults.classList.remove('hidden');
+      } else {
+        html = 'Sorry, no meal found with this ingredient';
       }
-      mealList.innerHTML = html;
+      mealList.innerHTML = resultList;
     });
+}
+
+// get recipe of selected meal
+getRecipe.addEventListener('click', recipeDetails);
+
+function recipeDetails(e) {
+  e.preventDefault();
+  if (e.target.id.contains('get-recipe')) {
+    let meal = e.target.parentElement;
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.dataset.id}`)
+      .then(res => res.json())
+      .then(data => recipeDetails(data.meals));
+    console.log(meal);
+  }
 }
