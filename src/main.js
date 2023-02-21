1d1dd7718874b538ae729982d8d991458fa8e9fe
Nanateng <nanateng2020@gmail.com>
2023-02-21 02:47:35 +0000

    changes to modal

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
-const hero = document.querySelector('.hero')
-const magnifyingGlass = document.querySelector('.magnifying-glass')
 const searchInput = document.querySelector('#search-input')
 const searchBtn = document.querySelector('button')
 const mealList = document.querySelector('#meal-card')
 const mealResults = document.querySelector('#result')
+const getRecipe = document.querySelector('#get-recipe')
 
-// nav search bar
-// magnifyingGlass.addEventListener('click', () => {
-//   searchInput.classList.toggle('hidden');
-//   magnifyingGlass.classList.add('invisible');
-// });
+//event listeners
+
 
 // get meal list with ingredient
 searchBtn.addEventListener('click', getMeals)
@@ -25,18 +21,13 @@ function getMeals() {
       if (data.meals) {
         data.meals.forEach(meal => {
           resultList += ` 
-<<<<<<< HEAD
-            <div class="flex flex-col rounded-xl w-72  py-3 meal-card" data-id="${meal.idMeal}" >
-=======
-          <div class="flex flex-wrap w-full flex-row">
-            <div class="flex flex-col rounded-xl py-3 meal-card" data-id="${meal.idMeal}" >
->>>>>>> dbf778ba32179eb7ce1496be546cc235aecd31af
-                <div class="w-72 h-72">
-                    <img class="rounded-lg object-cover w-full h-full drop-shadow-md" src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+            <div class="flex flex-col rounded-xl w-72  py-3 meal-card"  >
+                <div class="w-72 h-72"  >
+                    <img class="rounded-t-lg object-cover w-full h-full drop-shadow-md" src="${meal.strMealThumb}" alt="${meal.strMeal}" />
                 </div>
-                <div class="text-center font-body text-xl bg-black bg-opacity-30 rounded-b-md flex flex-col">
+                <div class="text-center font-body text-xl bg-black bg-opacity-30 rounded-b-md flex flex-col" data-id="${meal.idMeal}">
                     <span class="font-semibold">${meal.strMeal} </span>
-                    <span><a href="" class="text-cyan-300 italic underline leading-relaxed get-recipe" id="">Get Recipes</a></span>
+                    <span><a href="" class="text-cyan-300 italic underline leading-relaxed get-recipe" >Get Recipes</a></span>
                 </div>
             </div>
           </div>`
@@ -49,18 +40,23 @@ function getMeals() {
     })
 }
 
-const getRecipe = document.querySelector('#get-recipe')
+
 
 // get recipe of selected meal
-// getRecipe.addEventListener('click', recipeDetails)
+ mealList.addEventListener('click', getMealRecipe)
 
-function recipeDetails(e) {
-  e.preventDefault()
-  if (e.target.id.contains('get-recipe')) {
-    let meal = e.target.parentElement.parentElement
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.dataset.id}`)
-      .then(res => res.json())
-      .then(data => recipeDetails(data.meals))
-    console.log(meal)
+function getMealRecipe(e){
+  e.preventDefault();
+  if(e.target.classList.contains('get-recipe')){
+      let mealItem = e.target.parentElement.parentElement;
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+      .then(response => response.json())
+      .then(data => detailsModal(data.meals));
+      console.log(mealItem);
   }
+}
+
+function detailsModal(meal){
+  console.log(meal);
+  meal = meal[0]
 }
